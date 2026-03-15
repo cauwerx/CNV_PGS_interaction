9f66f0f4b5e3ca5717e680ddf3aadf099af5f7eb
scienception <cate.cevallos@gmail.com>
2026-03-15 21:48:35 +0100

    3k vs strict added

--- 04_model_assortative_mating/01_trait_correlation/03.couples.3k_vs_strict.py
+++ 04_model_assortative_mating/01_trait_correlation/03.couples.3k_vs_strict.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 from scipy.stats import pearsonr
 from scipy.stats import norm
-
+  
 project_DIR = "/mnt/project/"
 
 ##################################################################################################################
@@ -29,21 +29,31 @@ category_df = category_df.sort_values(by=["CATEGORY", "LABEL"], ascending=[True,
 ordered_phenotypes_abbrev = list(category_df['PHENO'])
 
 # 2. Read couples data
+# Jenny version
 couples = pd.read_csv(project_DIR + '/data/hh_pairs_filter.jenny_sjaarda.txt', sep='\t')
 couples = couples.astype('int32')
-
-# Condition to swap: if member1 is male, swap members so the member1 is female
 condition = couples['HOUSEHOLD_MEMBER1_sex'] == 1 # mask: is member1 male?
-# If condition holds, then swap members. Both operations have to be done at same time (swapping M to member2 and F to member1)
-# np.where chooses first option (swap), unless it's False for condition, in which case it chooses second option (don't swap)
 couples['HOUSEHOLD_MEMBER1'], couples['HOUSEHOLD_MEMBER2'] = np.where(condition, couples['HOUSEHOLD_MEMBER2'], couples['HOUSEHOLD_MEMBER1']), np.where(condition, couples['HOUSEHOLD_MEMBER1'], couples['HOUSEHOLD_MEMBER2'])
-# now all first members are F, so correct sex column as well
 couples['HOUSEHOLD_MEMBER1_sex'], couples['HOUSEHOLD_MEMBER2_sex'] = 0, 1
 
-# 3. Read phenotype data: WB and test set
+# Stricter version
+couples_strict = pd.read_csv(project_DIR+"/data/mate_pairs.yengo_def.txt", sep='\t')
+couples_strict = couples_strict[['ID1', 'ID2', 'sex_id1', 'sex_id2']]
+couples_strict = couples_strict.astype('int32')
+condition = couples_strict['sex_id1'] == 1 # mask: is member1 male?
+couples_strict['ID1'], couples_strict['ID2'] = np.where(condition, couples_strict['ID2'], couples_strict['ID1']), np.where(condition, couples_strict['ID1'], couples_strict['ID2'])
+couples_strict['sex_id1'], couples_strict['sex_id2'] = 0, 1
 
-# pheno_1: All white-british invidivuals
-pheno_1 = pd.read_csv(project_DIR+"/data/pheno/pheno_continuous_WB_INT_age_age2_sex_batch_PCs_All.tsv.tar.gz", sep='\t')
+# check no duplicate IDs
+assert len(couples['HOUSEHOLD_MEMBER1']) == len(set(couples['HOUSEHOLD_MEMBER1']))
+assert len(couples['HOUSEHOLD_MEMBER2']) == len(set(couples['HOUSEHOLD_MEMBER2']))
+assert len(couples_strict['ID1']) == len(set(couples_strict['ID1']))
+assert len(couples_strict['ID2']) == len(set(couples_strict['ID2']))
+
+# 3. Read phenotype data: WB and from test set
+
+# pheno_1: only test-set invidivuals 
+pheno_1 = pd.read_csv(project_DIR+"/data/pheno/pheno_continuous_test_INT_age_age2_sex_batch_PCs_All.tsv.tar.gz", sep='\t')
 pheno_F = pheno_1.copy()
 pheno_F = pheno_F[['IID']+ordered_phenotypes_abbrev]
 pheno_F.columns = [col + "_F" for col in pheno_F.columns]
@@ -52,14 +62,16 @@ pheno_M = pheno_1.copy()
 pheno_M = pheno_M[['IID']+ordered_phenotypes_abbrev]
 pheno_M.columns = [col + "_M" for col in pheno_M.columns]
 
-# Add phenotype data to couples df
+test_IDs = set(pheno_1['IID'])
+
+# Add phenotype data to Jenny couples
 couples_v1 = couples.copy()
 couples_v1 = pd.merge(couples_v1, pheno_F, left_on='HOUSEHOLD_MEMBER1', right_on='IID_F')
 couples_v1 = pd.merge(couples_v1, pheno_M, left_on='HOUSEHOLD_MEMBER2', right_on='IID_M')
-print("Number of couples in WB set:", len(couples_v1))
+print("Number of couples in test + Jenny set:", len(couples_v1))
 
-# pheno_2: only test-set individuals
-pheno_2 = pd.read_csv(project_DIR+"/data/pheno/pheno_continuous_test_INT_age_age2_sex_batch_PCs_All.tsv.tar.gz", sep='\t')
+# pheno_2: All white-british individuals
+pheno_2 = pd.read_csv(project_DIR+"/data/pheno/pheno_continuous_WB_INT_age_age2_sex_batch_PCs_All.tsv.tar.gz", sep='\t')
 pheno_F = pheno_2.copy()
 pheno_F = pheno_F[['IID']+ordered_phenotypes_abbrev]
 pheno_F.columns = [col + "_F" for col in pheno_F.columns]
@@ -68,11 +80,14 @@ pheno_M = pheno_2.copy()
 pheno_M = pheno_M[['IID']+ordered_phenotypes_abbrev]
 pheno_M.columns = [col + "_M" for col in pheno_M.columns]
 
-# Add phenotype data to couples df
-couples_v2 = couples.copy()
-couples_v2 = pd.merge(couples_v2, pheno_F, left_on='HOUSEHOLD_MEMBER1', right_on='IID_F')
-couples_v2 = pd.merge(couples_v2, pheno_M, left_on='HOUSEHOLD_MEMBER2', right_on='IID_M')
-print("Number of couples in test set:", len(couples_v2))
+# Add phenotype data to Yengo couples
+couples_v2 = couples_strict.copy()
+couples_v2 = pd.merge(couples_v2, pheno_F, left_on='ID1', right_on='IID_F')
+couples_v2 = pd.merge(couples_v2, pheno_M, left_on='ID2', right_on='IID_M')
+print("Number of couples in WB + strict set:", len(couples_v2))
+
+couples_v2 = couples_v2.query('ID1.isin(@test_IDs) & ID2.isin(@test_IDs)')
+print("Number of couples in test + strict set:", len(couples_v2))
 
 ##################################################################################################################
 # STEP 2: Compute within-couple phenotypic correlation in each set
@@ -81,12 +96,12 @@ print("Number of couples in test set:", len(couples_v2))
 # Initialize empty DataFrame
 corr_df = pd.DataFrame(
     index=ordered_phenotypes_abbrev,
-    columns=["pheno-pheno.50k.corr",  "pheno-pheno.50k.SE", "pheno-pheno.50k.p_val", "pheno-pheno.50k.n",
+    columns=["pheno-pheno.strict.corr",  "pheno-pheno.strict.SE", "pheno-pheno.strict.p_val", "pheno-pheno.strict.n",
              "pheno-pheno.3k.corr", "pheno-pheno.3k.SE", "pheno-pheno.3k.p_val", "pheno-pheno.3k.n"],
     dtype=float
 )
 
-# 50k
+# 3k
 for pheno in ordered_phenotypes_abbrev:
     col_F = f"{pheno}_F"
     col_M = f"{pheno}_M"
@@ -100,12 +115,12 @@ for pheno in ordered_phenotypes_abbrev:
             n = len(valid_data)
             se = pearson_se(r, n)
 
-            corr_df.loc[pheno, "pheno-pheno.50k.corr"] = r
-            corr_df.loc[pheno, "pheno-pheno.50k.SE"] = se
-            corr_df.loc[pheno, "pheno-pheno.50k.p_val"] = p
-            corr_df.loc[pheno, "pheno-pheno.50k.n"] = n
+            corr_df.loc[pheno, "pheno-pheno.3k.corr"] = r
+            corr_df.loc[pheno, "pheno-pheno.3k.SE"] = se
+            corr_df.loc[pheno, "pheno-pheno.3k.p_val"] = p
+            corr_df.loc[pheno, "pheno-pheno.3k.n"] = n
 
-# 3k
+# strict
 for pheno in ordered_phenotypes_abbrev:
     col_F = f"{pheno}_F"
     col_M = f"{pheno}_M"
@@ -119,13 +134,12 @@ for pheno in ordered_phenotypes_abbrev:
             n = len(valid_data)
             se = pearson_se(r, n)
 
-            corr_df.loc[pheno, "pheno-pheno.3k.corr"] = r
-            corr_df.loc[pheno, "pheno-pheno.3k.SE"] = se
-            corr_df.loc[pheno, "pheno-pheno.3k.p_val"] = p
-            corr_df.loc[pheno, "pheno-pheno.3k.n"] = n
+            corr_df.loc[pheno, "pheno-pheno.strict.corr"] = r
+            corr_df.loc[pheno, "pheno-pheno.strict.SE"] = se
+            corr_df.loc[pheno, "pheno-pheno.strict.p_val"] = p
+            corr_df.loc[pheno, "pheno-pheno.strict.n"] = n
 
-corr_df["pheno-pheno.50k.n"] = corr_df["pheno-pheno.50k.n"].astype('Int64')
+corr_df["pheno-pheno.strict.n"] = corr_df["pheno-pheno.strict.n"].astype('Int64')
 corr_df["pheno-pheno.3k.n"] = corr_df["pheno-pheno.3k.n"].astype('Int64')
 
-corr_df.to_csv('pheno_pheno.correlation.50k_vs_3k.csv')
-
+corr_df.to_csv('pheno_pheno.correlation.3k_vs_strict.csv')
